c9b5fff24d35c612b62034dc101f6513b42df987
Jithma Vithanage <76850026+jithmapv@users.noreply.github.com>
2025-05-05 16:41:34 +0530

    ✨Add reservation routes for starting, checking phone numbers, and listing reservations in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReservationsController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/reservation/start', [ReservationsController::class, 'start']);
+Route::post('/reservation/check-phone', [ReservationsController::class, 'checkPhone']);
+Route::get('/reservations', [ReservationsController::class, 'index']);
