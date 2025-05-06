498b6fa19ebeb7bf7e12521180a221263451a8a5
Jithma Vithanage <76850026+jithmapv@users.noreply.github.com>
2025-05-06 10:00:28 +0530

    ✨Enhance reservation routes by adding new endpoints for creating, storing, summarizing, and confirming reservations, as well as integrating order and payment functionalities.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReservationsController;
 use App\Http\Controllers\CustomerAuthController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PaymentController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,7 +13,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/reservation/start', [ReservationsController::class, 'start']);
-Route::post('/reservation/check-phone', [ReservationsController::class, 'checkPhone']);
+Route::get('/reservation/start', [ReservationsController::class, 'start'])->name('reservation.start');
+Route::post('/reservation/check-phone', [ReservationsController::class, 'checkPhone'])->name('reservation.checkPhone');
 Route::get('/reservations', [ReservationsController::class, 'index']);
 Route::get('/signup', [CustomerAuthController::class, 'showRegistrationForm'])->name('signup');
+Route::get('/reservation/create', [ReservationsController::class, 'create'])->name('reservation.create');
+Route::post('/reservation/store', [ReservationsController::class, 'store'])->name('reservation.store');
+Route::get('/reservation/{id}/summary', [ReservationsController::class, 'summary'])->name('reservation.summary');
+Route::post('/reservation/{id}/confirm', [ReservationsController::class, 'confirm'])->name('reservation.confirm');
+Route::get('/reservation/{id}/order', [OrderController::class, 'create'])->name('reservation.order');
+Route::get('/reservation/{id}/payment', [PaymentController::class, 'create'])->name('reservation.payment');
