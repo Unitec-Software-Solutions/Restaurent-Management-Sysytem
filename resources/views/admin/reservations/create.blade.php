1c1dddcf035f2988ec812c1e2d6ee6d5153fb8b5
Jithma Vithanage <76850026+jithmapv@users.noreply.github.com>
2025-05-16 17:59:53 +0530

    ✨Set default values for phone and date fields in reservation creation form

--- resources/views/admin/reservations/create.blade.php
+++ resources/views/admin/reservations/create.blade.php
@@ -39,6 +39,7 @@
                                 <input type="tel" 
                                        name="phone" 
                                        id="phone" 
+                                       value="{{ old('phone', $defaultPhone ?? '') }}"
                                        class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                                        required>
                             </div>
@@ -61,6 +62,7 @@
                                 <input type="date" 
                                        name="date" 
                                        id="date" 
+                                       value="{{ old('date', $defaultDate ?? '') }}"
                                        class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                                        required>
                             </div>
