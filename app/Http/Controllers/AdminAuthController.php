57e057e99b4498970f72cc566e6a00cce9640b05
Jithma Vithanage <76850026+jithmapv@users.noreply.github.com>
2025-05-16 13:47:43 +0530

    ✨Update logout redirection route in AdminAuthController for consistency

--- app/Http/Controllers/AdminAuthController.php
+++ app/Http/Controllers/AdminAuthController.php
@@ -35,6 +35,6 @@ class AdminAuthController extends Controller
         Auth::guard('admin')->logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect()->route('admin.auth.login');
+        return redirect()->route('admin.login');
     }
 }
