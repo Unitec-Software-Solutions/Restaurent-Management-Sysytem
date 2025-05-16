89718d7a126f4d13b5ead982f621ad187722006b
Jithma Vithanage <76850026+jithmapv@users.noreply.github.com>
2025-05-16 13:49:22 +0530

    ✨Refactor middleware in LoginController to ensure only authenticated users can access logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -31,7 +31,6 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
-        $this->middleware('auth')->only('logout');
+        $this->middleware('auth')->except('logout');
     }
 }
