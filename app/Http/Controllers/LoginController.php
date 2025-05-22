b68bed3cdef1e6ce52d4732d6400e3ac3d70aac5
Black-randy <59816942+Black-randy@users.noreply.github.com>
2025-05-22 14:03:48 +0530

    auth files updated

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    use AuthenticatesUsers;
+
+    /**
+     * Dynamically determine where to redirect users after login.
+     *
+     * @return string
+     */
+    protected function redirectTo()
+    {
+        if (Auth::guard('admin')->check()) {
+            return route('admin.dashboard'); // Redirect admins to admin dashboard
+        }
+
+        return '/home'; // Redirect regular users to user dashboard
+    }
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth')->except('logout');
+    }
+}
