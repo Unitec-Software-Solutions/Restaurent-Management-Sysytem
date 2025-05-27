1c242a771bb0b16b94ce019e1be3c871a98558a2
Jithma Vithanage <76850026+jithmapv@users.noreply.github.com>
2025-05-27 11:30:12 +0530

    ✨ feat: set default customer name in reservation creation form

--- resources/views/admin/reservations/create.blade.php
+++ resources/views/admin/reservations/create.blade.php
@@ -30,7 +30,8 @@
                                 <label for="name" class="block text-sm font-medium text-gray-700 mb-1">Customer Name</label>
                                 <input type="text" 
                                        name="name" 
-                                       id="name" 
+                                       id="name"
+                                       value="{{ old('name', $defaultName ?? '') }}"
                                        class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                                        required>
                             </div>
