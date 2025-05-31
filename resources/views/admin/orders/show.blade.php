f8ba7c5c6e12e65fcb5badd5d7775b3e042ff17b
Jithma Vithanage <76850026+jithmapv@users.noreply.github.com>
2025-05-31 17:50:39 +0530

    ✨ feat: update order show view to extend admin layout

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 @section('content')
 <div class="container mx-auto px-4 py-8">
     <div class="max-w-4xl mx-auto bg-white shadow-md rounded-lg p-6">
