99099298095fd4fb0b351bf50ea9af87067b2b02
Black-Randy <asiri.2704gg@gmail.com>
2025-06-05 14:55:54 +0530

    🗑️ Remove Suppliers Management link from breadcrumbs.

--- resources/views/admin/inventory/items/index.blade.php
+++ resources/views/admin/inventory/items/index.blade.php
@@ -8,7 +8,6 @@
             ['name' => 'Items Management', 'link' => route('admin.inventory.items.index')],
             ['name' => 'Stocks Management', 'link' => route('admin.inventory.stock.index')],
             ['name' => 'Transactions Management', 'link' => route('admin.inventory.stock.transactions.index')],
-            ['name' => 'Suppliers Management', 'link' => route('admin.suppliers.index')],
         ]" active="Items Management" />
 
         <!-- Header with KPI Cards -->
