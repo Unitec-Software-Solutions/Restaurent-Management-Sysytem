daead6abee203a51fb509b76bb803a0cffbbc021
Jithma Vithanage <76850026+jithmapv@users.noreply.github.com>
2025-06-11 17:15:13 +0530

    refactor: rename class from RedirectIfAuthenticated to AdminMiddleware for clarity

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class RedirectIfAuthenticated
+class AdminMiddleware
 {
     public function handle(Request $request, Closure $next, ...$guards)
     {
