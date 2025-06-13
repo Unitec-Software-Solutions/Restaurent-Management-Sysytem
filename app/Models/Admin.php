1a7bc1345e4e3cfaa6398e99e80f99a6f661ab8f
Jithma Vithanage <jpvithanage98@gmail.com>
2025-06-13 14:45:10 +0530

    feat: integrate HasRoles trait and fix organization relationship in Admin model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+use App\Models\Organization;
 
 class Admin extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasRoles;
 
     protected $fillable = [
         'name',
@@ -35,6 +37,15 @@ class Admin extends Authenticatable
      */
     public function organization()
     {
-        return $this->belongsTo(Organizations::class);
+        return $this->belongsTo(Organization::class);
+    }
+
+    /**
+     * Determine if the admin is a super admin.
+     * Adjust the logic as per your application's super admin identification.
+     */
+    public function isSuperAdmin()
+    {
+        return (bool) $this->is_superadmin; 
     }
 }
