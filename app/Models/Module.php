6f27a7e73d37420f0acf80cd59cbe729e2c7967d
Jithma Vithanage <jpvithanage98@gmail.com>
2025-06-13 14:45:21 +0530

    feat: add permissions relationship to Module model for module-permission association

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Module extends Model
 {
     protected $fillable = ['name', 'description'];
+
+    public function permissions()
+    {
+        return $this->belongsToMany(\Spatie\Permission\Models\Permission::class, 'module_permission');
+    }
 }
