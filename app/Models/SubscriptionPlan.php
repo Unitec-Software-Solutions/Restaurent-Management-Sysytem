94efc22a4c7f5f81fb290b3ea321211883973491
Jithma Vithanage <jpvithanage98@gmail.com>
2025-06-16 16:13:29 +0530

    ✨feat: add 'is_trial' field and update fillable attributes in SubscriptionPlan model

--- app/Models/SubscriptionPlan.php
+++ app/Models/SubscriptionPlan.php
@@ -6,8 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class SubscriptionPlan extends Model
 {
-    protected $fillable = ['name', 'modules', 'price', 'currency', 'description'];
+    protected $fillable = ['name', 'modules', 'price', 'currency', 'description', 'is_trial', 'trial_period_days'];
     protected $casts = [
         'modules' => 'array',
+        'is_trial' => 'boolean',
     ];
 }
