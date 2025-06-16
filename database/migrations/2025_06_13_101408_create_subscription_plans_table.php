34c4baba36ee462a6726ce66ef1e5f347673ff36
Jithma Vithanage <jpvithanage98@gmail.com>
2025-06-16 11:37:35 +0530

    ✨feat: create migration for subscription_plans table with necessary fields

---
+++ database/migrations/2025_06_13_101408_create_subscription_plans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscription_plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->json('modules'); // e.g. ["reservations","orders","inventory"]
+            $table->integer('price'); // in cents
+            $table->string('currency')->default('USD');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscription_plans');
+    }
+};
