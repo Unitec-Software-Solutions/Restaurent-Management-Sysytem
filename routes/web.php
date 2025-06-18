e1c412a76a7bfb813e5761af64619910be5e4f4e
Jithma Vithanage <jpvithanage98@gmail.com>
2025-06-18 10:44:57 +0530

    ✨feat: add user management routes for creating, editing, and assigning roles

--- routes/web.php
+++ routes/web.php
@@ -393,3 +393,17 @@ Route::middleware(['auth:admin', SuperAdmin::class])
         Route::post('roles/{role}/permissions', [\App\Http\Controllers\RoleController::class, 'updatePermissions'])->name('roles.permissions.update');
     });
 
+Route::middleware(['auth:admin', App\Http\Middleware\SuperAdmin::class])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+        Route::get('/users', [UserController::class, 'index'])->name('users.index');
+        Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+        Route::post('/users', [UserController::class, 'store'])->name('users.store');
+        Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+        Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+        Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+        Route::get('/users/{user}/assign-role', [UserController::class, 'assignRoleForm'])->name('users.assign-role');
+        Route::post('/users/{user}/assign-role', [UserController::class, 'assignRole'])->name('users.assign-role.store');
+    });
+
