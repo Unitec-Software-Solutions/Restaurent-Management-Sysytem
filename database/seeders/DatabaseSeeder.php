86620961ce3665f7d807e2d054fb22921e45b7d5
Jithma Vithanage <jpvithanage98@gmail.com>
2025-06-19 15:20:46 +0530

    ✨feat: add UserSeeder to create various user roles and update LoginSeeder for super admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -36,6 +36,7 @@ class DatabaseSeeder extends Seeder
             SuperAdminSeeder::class,
             ModulesTableSeeder::class,
             RoleSeeder::class,
+            UserSeeder::class,
 
         ]);
     }
