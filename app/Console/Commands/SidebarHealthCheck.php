6824b2e551d11f287ac201bfef3257a946505699
Jithma Vithanage <jpvithanage98@gmail.com>
2025-06-24 12:29:46 +0530

    :refactor: update SidebarHealthCheck command imports for improved clarity

--- app/Console/Commands/SidebarHealthCheck.php
+++ app/Console/Commands/SidebarHealthCheck.php
@@ -4,9 +4,10 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Schema;
-use App\Models\Admin;
+use Illuminate\Support\Facades\DB;
 
 class SidebarHealthCheck extends Command
 {
