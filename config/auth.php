543cef16d00aaac80c9c99d7a732750128a78356
Jithma Vithanage <jpvithanage98@gmail.com>
2025-06-24 12:31:22 +0530

    :refactor: update auth configuration to include admin user provider

--- config/auth.php
+++ config/auth.php
@@ -39,7 +39,7 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-
+        
         'admin' => [
             'driver' => 'session',
             'provider' => 'admins',
@@ -68,7 +68,7 @@ return [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
         ],
-
+        
         'admins' => [
             'driver' => 'eloquent',
             'model' => App\Models\Admin::class,
