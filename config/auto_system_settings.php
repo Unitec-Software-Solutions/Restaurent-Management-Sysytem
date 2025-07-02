cdd669ec394bf7b01774315e8e6e6e992628b6b6
Jithma Vithanage <jpvithanage98@gmail.com>
2025-07-02 11:24:38 +0530

    :seedling: feat: add auto-generated system settings configuration file

---
+++ config/auto_system_settings.php
@@ -0,0 +1,15 @@
+<?php
+
+// Auto-generated system settings
+return [
+    'app_name' => 'Restaurant Management System',
+    'default_currency' => 'USD',
+    'currency_symbol' => '$',
+    'date_format' => 'Y-m-d',
+    'default_service_charge' => '10.00',
+    'default_tax_rate' => '7.50',
+    'enable_table_reservations' => 'true',
+    'default_preparation_time' => '15',
+    'enable_takeaway' => 'true',
+    'enable_delivery' => 'true',
+];
