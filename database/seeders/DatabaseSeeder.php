3ea2af60105ce52bf33218cb9cd9c7a92d0d60bb
Jithma Vithanage <jpvithanage98@gmail.com>
2025-07-02 09:47:50 +0530

    Add seeders for various database entities
    - Created AdminSeeder, AuditLogSeeder, BillSeeder, BranchSeeder, CustomRoleSeeder, CustomerSeeder, EmployeeSeeder, GoodsTransferItemSeeder, GoodsTransferNoteSeeder, GrnItemSeeder, GrnMasterSeeder, InventoryItemSeeder, ItemCategorySeeder, ItemMasterSeeder, ItemTransactionSeeder, KitchenStationSeeder, KotItemSeeder, KotSeeder, MenuCategorySeeder, MenuItemSeeder, MenuSeeder, ModuleSeeder, NotificationProviderSeeder, OrderItemSeeder, OrderSeeder, OrderStatusStateMachineSeeder, OrganizationSeeder, PaymentAllocationSeeder, PaymentSeeder, PermissionSeeder, ProductionOrderIngredientSeeder, ProductionOrderItemSeeder, ProductionOrderSeeder, ProductionRecipeDetailSeeder, ProductionRecipeSeeder, ProductionRequestItemSeeder, ProductionRequestMasterSeeder, ProductionSessionSeeder, PurchaseOrderItemSeeder, PurchaseOrderSeeder, RecipeSeeder, ReservationCancellationMailSeeder, ReservationSeeder, RestaurantConfigSeeder, RoleSeeder, ShiftAssignmentSeeder, ShiftSeeder, StaffSeeder, StockReservationSeeder, SubscriptionPlanSeeder, SubscriptionSeeder, SupplierPaymentDetailSeeder, SupplierPaymentMasterSeeder, SupplierSeeder, SystemSettingSeeder, TableSeeder, UserSeeder.
    - Updated DatabaseSeeder to include all new seeders.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +11,64 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $this->call([
+            AuditLogSeeder::class,
+            EmployeeSeeder::class,
+            GrnItemSeeder::class,
+            InventoryItemSeeder::class,
+            ItemMasterSeeder::class,
+            KitchenStationSeeder::class,
+            KotItemSeeder::class,
+            MenuSeeder::class,
+            MenuItemSeeder::class,
+            NotificationProviderSeeder::class,
+            OrderSeeder::class,
+            OrderStatusStateMachineSeeder::class,
+            PaymentSeeder::class,
+            PaymentAllocationSeeder::class,
+            ProductionOrderSeeder::class,
+            ProductionOrderIngredientSeeder::class,
+            ProductionRecipeSeeder::class,
+            ProductionRequestItemSeeder::class,
+            ProductionSessionSeeder::class,
+            RecipeSeeder::class,
+            ReservationCancellationMailSeeder::class,
+            ShiftSeeder::class,
+            StockReservationSeeder::class,
+            SubscriptionPlanSeeder::class,
+            SupplierPaymentDetailSeeder::class,
+            SystemSettingSeeder::class,
+            UserSeeder::class,
+            TableSeeder::class,
+            SupplierPaymentMasterSeeder::class,
+            SupplierSeeder::class,
+            SubscriptionSeeder::class,
+            StaffSeeder::class,
+            ShiftAssignmentSeeder::class,
+            RoleSeeder::class,
+            RestaurantConfigSeeder::class,
+            ReservationSeeder::class,
+            PurchaseOrderItemSeeder::class,
+            PurchaseOrderSeeder::class,
+            ProductionRequestMasterSeeder::class,
+            ProductionRecipeDetailSeeder::class,
+            ProductionOrderItemSeeder::class,
+            PermissionSeeder::class,
+            OrderItemSeeder::class,
+            OrganizationSeeder::class,
+            ModuleSeeder::class,
+            MenuCategorySeeder::class,
+            KotSeeder::class,
+            GrnMasterSeeder::class,
+            ItemTransactionSeeder::class,
+            ItemCategorySeeder::class,
+            GoodsTransferNoteSeeder::class,
+            GoodsTransferItemSeeder::class,
+            CustomRoleSeeder::class,
+            CustomerSeeder::class,
+            BranchSeeder::class,
+            BillSeeder::class,
+            AdminSeeder::class,
+        ]);
     }
 }
---
+++ database/seeders/ProductionRequestItemSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductionRequestItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
