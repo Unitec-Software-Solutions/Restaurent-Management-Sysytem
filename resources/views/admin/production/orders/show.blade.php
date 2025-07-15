b8c8eef3f672964a544668540c62f38b74741b56
Black-Randy <asiri.2704gg@gmail.com>
2025-07-15 17:51:16 +0530

    ✨ style: update header title definition in production order details view for consistency

--- resources/views/admin/production/orders/show.blade.php
+++ resources/views/admin/production/orders/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('title', 'Production Order Details')
-
+@section('header-title', 'Production Order Details')
 @section('content')
     <div class="container mx-auto px-4 py-8">
         <div class="max-w-6xl mx-auto">
