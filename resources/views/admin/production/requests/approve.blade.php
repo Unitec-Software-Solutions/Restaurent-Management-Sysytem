19169e14ec16f4042318668d306cc3f431a03c93
Black-Randy <asiri.2704gg@gmail.com>
2025-07-18 18:03:49 +0530

    🛠 fix(approve): remove commented-out ingredient management link

--- resources/views/admin/production/requests/approve.blade.php
+++ resources/views/admin/production/requests/approve.blade.php
@@ -15,7 +15,7 @@
                     ['name' => 'Production Orders', 'link' => route('admin.production.orders.index')],
                     ['name' => 'Production Sessions', 'link' => route('admin.production.sessions.index')],
                     ['name' => 'Production Recipes', 'link' => route('admin.production.recipes.index')],
-                    ['name' => 'Ingredient Management', 'link' => '#', 'disabled' => true],
+                    // ['name' => 'Ingredient Management', 'link' => '#', 'disabled' => true],
                 ]" active="Production Requests" />
             </div>
         </div>
