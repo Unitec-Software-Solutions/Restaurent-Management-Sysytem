b50c90c3eeae61aab4cb305344b087f01398a9dc
Jithma Vithanage <76850026+jithmapv@users.noreply.github.com>
2025-07-18 17:08:35 +0530

    Add AdminsTableSeeder and RolesTableSeeder to DatabaseSeeder for complete initial data setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,5 +23,7 @@ class DatabaseSeeder extends Seeder
         $this->command->info('🔐 Login at /admin/login with: superadmin@rms.com / SuperAdmin123!');
         $this->call(OrganizationsTableSeeder::class);
         $this->call(BranchesTableSeeder::class);
+        $this->call(AdminsTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
     }
 }
