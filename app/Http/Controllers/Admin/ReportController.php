d1c59bfee4ebc355fa9e196c4b020672d645438b
Black-Randy <asiri.2704gg@gmail.com>
2025-07-21 16:24:25 +0530

    ✨ feat(ReportController): implement sales and inventory report methods Added methods for sales and inventory reports in ReportController.php

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -9,8 +9,38 @@ class ReportController extends Controller
 {
     public function index()
     {
-        // TODO: Implement index logic
         return view('admin.reports.index');
     }
+
+    public function salesReport()
+    {
+        return view('admin.reports.sales.index');
+    }
+
+    public function inventoryReport()
+    {
+        return view('admin.reports.inventory.index');
+    }
+
+    public function inventoryGrn()
+    {
+        return view('admin.reports.inventory.grn.index');
+    }
+
+    public function inventoryGtn()
+    {
+        return view('admin.reports.inventory.gtn.index');
+    }
+
+    public function inventorySrn()
+    {
+        return view('admin.reports.inventory.srn.index');
+    }
+
+    public function inventoryStock()
+    {
+        return view('admin.reports.inventory.stock.index');
+    }
+
 
 }
