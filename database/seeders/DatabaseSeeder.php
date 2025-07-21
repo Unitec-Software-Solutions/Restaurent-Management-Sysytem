5268fd383fc328cb2dfb2af72a697119080e0e73
Jithma Vithanage <76850026+jithmapv@users.noreply.github.com>
2025-07-21 15:01:40 +0530

    Refactor DatabaseSeeder to remove unnecessary seeders and streamline the seeding process

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,10 +21,6 @@ class DatabaseSeeder extends Seeder
 
         $this->command->info('✅ Minimal seeding completed successfully');
         $this->command->info('🔐 Login at /admin/login with: superadmin@rms.com / SuperAdmin123!');
-        $this->call(OrganizationsTableSeeder::class);
-        $this->call(BranchesTableSeeder::class);
-        $this->call(AdminsTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
-        $this->call(PermissionsTableSeeder::class);
+        $this->call(SystemPermissionsSeeder::class);
     }
 }
