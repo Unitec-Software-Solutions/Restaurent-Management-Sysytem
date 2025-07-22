08611653f4bf048ca08a5b018163b932fa5557a9
Black-Randy <asiri.2704gg@gmail.com>
2025-07-22 14:17:26 +0530

    ✨ feat(inventory-report): enhance navigation buttons layout

--- resources/views/admin/reports/inventory/index.blade.php
+++ resources/views/admin/reports/inventory/index.blade.php
@@ -9,11 +9,12 @@
         <div class="rounded-lg">
             <x-nav-buttons :items="[
                 ['name' => '<< Back', 'link' => route('admin.reports.index')],
-                ['name' => 'Inventory Report', 'link' => route('admin.reports.inventory.index')],
-                ['name' => 'Goods Transfer Note Report', 'link' => route('admin.reports.inventory.gtn')],
-                ['name' => 'Goods Receipt Note Report', 'link' => route('admin.reports.inventory.grn')],
-                ['name' => 'Stock Release Note Report', 'link' => route('admin.reports.inventory.srn')],
-                ['name' => 'Stock Report', 'link' => route('admin.reports.inventory.stock')],
+            ['name' => 'Inventory Report', 'link' => route('admin.reports.inventory.index')],
+            ['name' => 'Stock Report', 'link' => route('admin.reports.inventory.stock')],
+            ['name' => 'Category Report', 'link' => route('admin.reports.inventory.category')],
+            ['name' => 'Goods Transfer Note Report', 'link' => route('admin.reports.inventory.gtn')],
+            ['name' => 'Goods Receipt Note Report', 'link' => route('admin.reports.inventory.grn')],
+            ['name' => 'Stock Release Note Report', 'link' => route('admin.reports.inventory.srn')],
                 // ['name' => 'Inventory items Report', 'link' => route('admin.reports.inventory.items.index')],
             ]" active="Inventory Report" />
         </div>
